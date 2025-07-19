fcc409a9dc4913df9ca2ae8478249088afba033e
Anesu Kafesu <a.kafesu@alustudent.com>
2025-07-19 13:45:54 +0200

    fixes

--- chat/neighbourhoods.py
+++ chat/neighbourhoods.py
@@ -3,28 +3,28 @@ from supabase_client import client
 
 class Neighbourhoods:
   def __init__(self):
-    self['neighbourhoods'] = {}
+    self.neighbourhoods = {}
 
     neighbourhoods = client.table('neighbourhoods').select('id,name').execute()
     for neighbourhood in neighbourhoods:
       id = neighbourhood['id']
       name = neighbourhood['name'].lower()
-      self['neighbourhoods'][name] = id
+      self.neighbourhoods[name] = id
 
     return self
   
   def delete_neighbourhood(self, id):
-    self['neighbourhoods'] = { k: v for k, v in self['neighbourhoods'] if v != id }
+    self.neighbourhoods = { k: v for k, v in self.neighbourhoods if v != id }
   
   def update_neighourhood(self, name, id):
     self.delete_neighbourhood(id)
     self.add_neighourhood(name, id)
   
   def add_neighourhood(self, name, id):
-    self['neighbourhoods'][name.lower()] = id
+    self.neighbourhoods[name.lower()] = id
   
   def get_neighbourhood_id(self, name):
-    return self['neighbourhoods'].get(name.lower())
+    return self.neighbourhoods.get(name.lower())
 
 
 neighbourhood_lookup = Neighbourhoods()
