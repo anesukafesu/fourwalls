e285be487f2590d0fc22342effc54d09b00250e4
Anesu Kafesu <a.kafesu@alustudent.com>
2025-07-19 13:48:59 +0200

    chat fixe

--- chat/neighbourhoods.py
+++ chat/neighbourhoods.py
@@ -7,21 +7,24 @@ class Neighbourhoods:
 
     neighbourhoods = client.table('neighbourhoods').select('id,name').execute()
     for neighbourhood in neighbourhoods:
-      id = neighbourhood['id']
-      name = neighbourhood['name'].lower()
-      self.neighbourhoods[name] = id
-
+      id = neighbourhood[0]
+      name = neighbourhood[1].lower()
+      self.add_neighourhood(name, id)
+    print(self.neighbourhoods)
     return self
   
   def delete_neighbourhood(self, id):
     self.neighbourhoods = { k: v for k, v in self.neighbourhoods if v != id }
+    print(self.neighbourhoods)
   
   def update_neighourhood(self, name, id):
     self.delete_neighbourhood(id)
     self.add_neighourhood(name, id)
+    print(self.neighbourhoods)
   
   def add_neighourhood(self, name, id):
     self.neighbourhoods[name.lower()] = id
+    print(self.neighbourhoods)
   
   def get_neighbourhood_id(self, name):
     return self.neighbourhoods.get(name.lower())
