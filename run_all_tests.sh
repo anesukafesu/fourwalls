2bba5ee89a1f195fc36c3a8b02a9d3ddf59152d4
Anesu Kafesu <a.kafesu@alustudent.com>
2025-07-24 17:42:11 +0200

    all tests

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -2,10 +2,11 @@
 
 # Run Python tests (assumes pytest is used and all python test folders are in the root)
 echo "Running Python tests..."
+cd tests
 pytest || exit 1
 
 # Run marketplace (frontend) tests
-cd marketplace || exit 1
+cd ../marketplace || exit 1
 echo "Running marketplace (frontend) tests..."
 # Use npm, pnpm, or yarn as appropriate. Here we use npm:
 npm test || exit 1
